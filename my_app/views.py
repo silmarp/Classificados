8b2a09bf8b6366f5cf7e149261ecd36e40899149
Silmar Pereira da Silva Junior <silmarjr2@gmail.com>
2020-09-27 02:27:57 -0300

    Imagens resolvidas
    não deu certo usar OLX então ajeitei como iam funcionar as imagens no site

--- my_app/views.py
+++ my_app/views.py
@@ -7,6 +7,7 @@ from requests.compat import quote_plus
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -32,8 +33,13 @@ def new_search(request):
             post_price = post.find(class_= 'result-price').text
         else:
             post_price = 'N/A'
-        #### ADICIONAR COMO CHEGAR AS IMAGENS 
-        if post.find(class_=)
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
 
         final_postings.append((post_title, post_url, post_price, post_image_url))
 
